fb5871847e743337365c8281b6523df26ded307f
vadim <vadimbelaev002@gmail.com>
2024-04-26 17:12:55 +0300

    init commit

---
+++ src/main/kotlin/com/andersen/manageclients/ManageClientsApplication.kt
@@ -0,0 +1,11 @@
+package com.andersen.manageclients
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ManageClientsApplication
+
+fun main(args: Array<String>) {
+    runApplication<ManageClientsApplication>(*args)
+}
