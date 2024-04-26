53b7a296b8d610dd1c01e26e0ee43b11bb3b9fcd
vadim <vadimbelaev002@gmail.com>
2024-04-26 19:29:52 +0300

    created and documented endpoints in openapi.yaml and implemented these methods in delegate

--- src/main/kotlin/com/andersen/manageclients/delegate/ClientApiDelegateImpl.kt
+++ src/main/kotlin/com/andersen/manageclients/delegate/ClientApiDelegateImpl.kt
@@ -2,22 +2,35 @@ package com.andersen.manageclients.delegate
 
 import com.andersen.manageclients.service.ClientService
 import org.openapitools.client.api.ClientsApiController
+import org.openapitools.client.model.ClientRequestDto
+import org.openapitools.client.model.ClientResponseDto
+
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
+import java.util.UUID
 
 @Component
 class ClientApiDelegateImpl(
         private val clientService: ClientService
 ) : ClientsApiController() {
 
+    override fun getClient(clientId: String): ResponseEntity<ClientResponseDto> {
+        return ResponseEntity.status(HttpStatus.OK).body(clientService.getById(UUID.fromString(clientId)))
+    }
+    override fun getAllClients(): ResponseEntity<List<ClientResponseDto>> {
+        return ResponseEntity.status(HttpStatus.OK).body(clientService.getAll())
+    }
 
-//    override fun getClient(clientId: String): ResponseEntity<ClientResponseDto> {
-//        val clientUuid= UUID.fromString(clientId);
-//        return ResponseEntity.status(HttpStatus.OK).body(clientService.getById(clientUuid))
-//    }
+    override fun deleteClient(clientId: String): ResponseEntity<Unit> {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
+    }
 
-    override fun getAllClients(): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.OK).body("aaa")
+    override fun saveClient(clientRequestDto: ClientRequestDto): ResponseEntity<ClientResponseDto> {
+        return ResponseEntity.status(HttpStatus.CREATED).body(clientService.save(clientRequestDto));
+    }
+
+    override fun updateClient(clientId: String, clientRequestDto: ClientRequestDto): ResponseEntity<ClientResponseDto> {
+        return ResponseEntity.status(HttpStatus.OK).body(clientService.update(UUID.fromString(clientId), clientRequestDto))
     }
 }
